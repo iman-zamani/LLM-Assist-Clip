5e1f8a7fda6d9f3ad8d1f547636ad9fc790ce87a
imzzzzzz <izamanimoghaddam@gmail.com>
2024-09-09 03:14:16 +0330

    add recursive functionality

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ std::string readFileContent(const std::string& filePath) {
 }
 
 // this function is for getting the path to all files in the given directory with the specified type 
-std::vector<std::string> getFilesByType(const std::string& dirPath, const std::string& fileType) {
+std::vector<std::string> getFilesByTypeRecursive(const std::string& dirPath, const std::string& fileType) {
     std::vector<std::string> filePaths;
     std::filesystem::path directory(dirPath);
 
@@ -34,6 +34,32 @@ std::vector<std::string> getFilesByType(const std::string& dirPath, const std::s
     // recursive file search
     try {
         for (const auto& entry : std::filesystem::recursive_directory_iterator(directory)) {
+            if (entry.is_regular_file() && entry.path().extension() == fileType) {
+                // Compute relative path from the given directory
+                std::filesystem::path relativePath = std::filesystem::relative(entry.path(), directory);
+                filePaths.push_back(relativePath.string());  
+            }
+        }
+    } catch (const std::filesystem::filesystem_error& e) {
+        std::cerr << "Directory access error: " << e.what() << std::endl;
+    }
+
+    return filePaths;
+}
+
+std::vector<std::string> getFilesByType(const std::string& dirPath, const std::string& fileType) {
+    std::vector<std::string> filePaths;
+    std::filesystem::path directory(dirPath);
+
+    // check directory validity
+    if (!std::filesystem::exists(directory) || !std::filesystem::is_directory(directory)) {
+        std::cerr << "Error accessing the directory: " << dirPath << std::endl;
+        return filePaths;
+    }
+
+    // non-recursive file search
+    try {
+        for (const auto& entry : std::filesystem::directory_iterator(directory)) {
             if (entry.is_regular_file() && entry.path().extension() == fileType) {
                 // Compute relative path from the given directory
                 std::filesystem::path relativePath = std::filesystem::relative(entry.path(), directory);
@@ -64,16 +90,24 @@ int main(int argc, char *argv[]){
         std::cerr<<"you need to specify extensions of the files you want"<<std::endl;;
         return EXIT_FAILURE;
     }
+    bool recursive = false;
     
     // reading the file types that user want 
     for (int i=1;i<argc;i++){
         std::string extension = std::string(argv[i]);
+        // check to see where should we look recursively 
+        if (extension == "-r"){
+            recursive = true;
+            continue;
+        }
         // add '.' to extension specified by user if it doesn't have one  
         if (extension.at(0)!='.'){
             extension = "." + extension;
         }
         //get the list of all the files with that extension
-        std::vector<std::string> filesWithThisExtension = getFilesByType(dirPath,extension);
+        std::vector<std::string> filesWithThisExtension;
+        if (recursive) filesWithThisExtension = getFilesByTypeRecursive(dirPath,extension);
+        else filesWithThisExtension = getFilesByType(dirPath,extension);
         if (filesWithThisExtension.empty()){continue;}
         for (std::string &file : filesWithThisExtension){
             clipboard += file + ":\n\n\n";
